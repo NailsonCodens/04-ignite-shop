2897d51ff7721ec55965f8a4a99347520cab2587
Nailson <nailson.ivs@codens.com.br>
2023-11-10 22:32:50 -0300

    :sparkles: :product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,19 @@
 import { useRouter } from 'next/router'
+import { ImageContainer, ProductContainer, ProductDetails } from '../../../styles/pages/product'
 
 export default function Product(){
   const {query} = useRouter()
 
   return (
-    <h1>product {query.id}</h1>
+    <ProductContainer>
+      <ImageContainer>
+      </ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta x</h1>
+        <span>R$ 79,00</span>
+        <p>Lorem ipsum</p>
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer> 
   )
 }
